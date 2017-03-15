46c21198e3a89831791a39d05378bfdfa55c3d0e
JD <davis3792@yahoo.com>
2017-03-15 10:30:33 -0400

    stuff

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
+var express = require('express');
+var app = express();
 
-var app = connect();
+var apiEndpoint = process.env.MISTORY_API_ENDPOINT;
+if (!apiEndpoint) {
+	console.log('MISTORY_API_ENDPOINT environment variable not set. Exiting');
+	process.exit(1);
+}
 
-app.use(serveStatic(__dirname, {'index': ['index.html']}));
+app.get('/config', function(req, res){
+  res.send({'apiEndpoint': apiEndpoint});
+});
+
+app.use(express.static(__dirname));
 app.listen(3000);
-
